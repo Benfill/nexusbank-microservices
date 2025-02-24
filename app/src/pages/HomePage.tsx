56716773873f06b2d46d8e1d4294909da27368d1
Anass Benfillous <anasse.benfillous@gmail.com>
2025-02-24 15:00:25 +0000

    add onDelete method

--- app/src/pages/HomePage.tsx
+++ app/src/pages/HomePage.tsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { deleteCustomer, getCustomers } from "../api/customerService";
 import Table, { Column } from "../components/Table";
-import { getAccounts } from "../api/accountService";
+import { deleteAccount, getAccounts } from "../api/accountService";
 import Customer from "../types/customerTypes";
+import Alert from "@mui/material/Alert";
+import Account from "../types/accountTypes";
 
 const HomePage: React.FC = () => {
-  const [customers, setCustomers] = React.useState([]);
-  const [accounts, setAccounts] = React.useState([]);
+  const [customers, setCustomers] = useState([]);
+  const [accounts, setAccounts] = useState([]);
+  const [isShown, setIsShowen] = useState(false);
+  const [message, setMessage] = useState("");
+  const [severity, setSeverity] = useState<"success" | "error" | undefined>();
 
   React.useEffect(() => {
     const fetchCustomers = async () => {
@@ -20,18 +25,50 @@ const HomePage: React.FC = () => {
 
     fetchCustomers();
     fetchAccount();
-  }, []);
+  }, [isShown]);
 
   const customerDelete = async (customer: Customer) => {
     try {
-      await deleteCustomer(customer); // or whatever parameter your deleteCustomer function expects
-      // Optionally refresh your data or show a success message
+      await deleteCustomer(customer);
+      setIsShowen(true);
+      setMessage("Customer deleted successfully");
+      setSeverity("success");
     } catch (error) {
       // Handle any errors that occur during deletion
       console.error("Failed to delete customer:", error);
-      // Optionally show an error message to the user
+      setIsShowen(true);
+      setMessage("Failed to delete customer:" + error);
+      setSeverity("error");
+    } finally {
+      setTimeout(() => {
+        setIsShowen(false);
+        setMessage("");
+        setSeverity(undefined);
+      }, 5000);
     }
   };
+
+  const accountDelete = async (account: Account) => {
+    try {
+      await deleteAccount(account);
+      setIsShowen(true);
+      setMessage("Account deleted successfully");
+      setSeverity("success");
+    } catch (error) {
+      // Handle any errors that occur during deletion
+      console.error("Failed to delete account:", error);
+      setIsShowen(true);
+      setMessage("Failed to delete account:" + error);
+      setSeverity("error");
+    } finally {
+      setTimeout(() => {
+        setIsShowen(false);
+        setMessage("");
+        setSeverity(undefined);
+      }, 5000);
+    }
+  };
+
   const customerColumns: Column[] = [
     {
       key: "id",
@@ -96,11 +133,17 @@ const HomePage: React.FC = () => {
       label: "Updated At",
       type: "date",
     },
+    {
+      key: "actions",
+      label: "Actions",
+      type: "actions",
+    },
   ];
 
   return (
     <div className="px-40 py-10 flex flex-col gap-10">
       <h1 className="text-center">Dashboard</h1>
+      {isShown && <Alert severity={severity}> {message && message} </Alert>}
 
       <div>
         <h2>Customers</h2>
@@ -113,7 +156,11 @@ const HomePage: React.FC = () => {
 
       <div>
         <h2>Accounts</h2>
-        <Table columns={accountColumns} data={accounts}></Table>
+        <Table
+          columns={accountColumns}
+          data={accounts}
+          onDelete={accountDelete}
+        ></Table>
       </div>
     </div>
   );
